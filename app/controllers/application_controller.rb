a2447653f4303081f892edbfcc37914ee831cbd0
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-05 01:53:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :home
+    end
 
+    post '/teams' do
+      @member1 = params["team"]["members"][0]
+      @member2 = params["team"]["members"][1]
+      @member3 = params["team"]["members"][2]
+      @members = [@member1, @member2, @member3]
+      @team = params["team"]
+      erb :team
+    end
 end
