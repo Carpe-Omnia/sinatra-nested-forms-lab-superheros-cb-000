c9b168b01bb5274ebb02deb066ebcf6ddecac3b5
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-05 02:06:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,9 @@ class App < Sinatra::Base
       @member1 = params["team"]["members"]["0"]
       puts @member1
       @member2 = params["team"]["members"]["1"]
-
+      puts @member2
       @member3 = params["team"]["members"]["2"]
+      puts @member3
       @team = params["team"]
       erb :team
     end
